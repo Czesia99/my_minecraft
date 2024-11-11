166a3413ebbe6885eb30aa0bd72cf8fdacb821b3
yann <yann.r.berger@gmail.com>
2024-11-11 23:50:17 +0100

    imgui preparation

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,22 @@
 
 #include "GameScene.hpp"
 
+// #include <imgui.h>
+// #include <imgui_impl_glfw.h>
+// #include <imgui_impl_opengl3.h>
+
 int main()
 {
+
     mygl::Context ctx(1920, 1080, "MY_MINECRAFT");
     glfwSwapInterval(1); //vsync
+
+    //IMGUI
+    // IMGUI_CHECKVERSION();
+    // ImGui::CreateContext();
+    // ImGui_ImplGlfw_InitForOpenGL(ctx.window, true);
+    // ImGui_ImplOpenGL3_Init();
+    //IMGUI END
 
     game::GameScene game(ctx); //idx 0
 
